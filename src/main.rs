9907f1efd5eac1981788c8cadcfa8b8aaa33442f
Konstantinos Keramaris <17270464+konkerama@users.noreply.github.com>
2023-08-19 13:24:05 +0300

    Upgrades (#1)
    * cargo fmt
    
    * upgrades
    
    * modified readme

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
-mod mongo;
 mod error;
 mod handler;
 mod model;
-mod response;
-mod schema;
+mod mongo;
 mod pg;
+mod response;
 mod route;
+mod schema;
 
 pub use self::error::{Error, Result};
 
+use autometrics::prometheus_exporter;
 use mongo::MONGO;
 use pg::PG;
 use route::create_router;
-use tracing_subscriber::{layer::SubscriberExt, Registry};
 use tracing::level_filters::LevelFilter;
 use tracing::Level;
-
+use tracing_subscriber::{layer::SubscriberExt, Registry};
 
 #[tokio::main]
 async fn main() {
+    prometheus_exporter::init();
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "app=info,tower_http=trace");
     }
@@ -42,38 +43,35 @@ async fn main() {
         .unwrap();
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::{response::*, schema::*};
+    use axum::http::Method;
     use axum::{
         body::Body,
-        Router,
         http::{self, Request, StatusCode},
+        Router,
     };
     use serde_json::json;
     use tower::ServiceExt; // for `oneshot` and `ready`
-    use crate::{
-        response::*,schema::*
-    };
-    use axum::http::Method;
 
-    async fn init() -> Router{
+    async fn init() -> Router {
         let pg = PG::init().await.unwrap();
         let mongo = MONGO::init().await.unwrap();
 
         create_router(pg.clone(), mongo.clone())
     }
 
-    fn get_customer_model(name: &str, surname: &str) -> CreateCustomerSchema{
-        CreateCustomerSchema{
+    fn get_customer_model(name: &str, surname: &str) -> CreateCustomerSchema {
+        CreateCustomerSchema {
             customer_name: name.to_string(),
             customer_surname: surname.to_string(),
         }
     }
 
-    fn get_order_schema(customer_name: &str, product_name: &str) -> CreateOrderSchema{
-        CreateOrderSchema{
+    fn get_order_schema(customer_name: &str, product_name: &str) -> CreateOrderSchema {
+        CreateOrderSchema {
             customer_name: customer_name.to_string(),
             product_name: product_name.to_string(),
         }
@@ -88,7 +86,8 @@ mod tests {
                     .uri(uri)
                     .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
                     .body(body)
-                    .unwrap())
+                    .unwrap(),
+            )
             .await
             .unwrap();
         let status = response.status();
@@ -98,54 +97,85 @@ mod tests {
     }
     #[tokio::test]
     async fn health_check() {
-        let (status_code, response)  = api_call(http::Method::GET, "/api/healthchecker", Body::empty()).await;
+        let (status_code, response) =
+            api_call(http::Method::GET, "/api/healthchecker", Body::empty()).await;
         assert_eq!(status_code, StatusCode::OK);
         let expected_json = GenericResponse {
             status: "success".to_string(),
             message: "Build CRUD API with Rust and MongoDB".to_string(),
         };
 
-        assert_eq!(response.get("message").unwrap().as_str().unwrap(), expected_json.message);
+        assert_eq!(
+            response.get("message").unwrap().as_str().unwrap(),
+            expected_json.message
+        );
     }
 
     #[tokio::test]
     async fn create_customer() {
         let input = get_customer_model("paul", "doe");
 
-        let (status_code, response)  = api_call(http::Method::POST, "/api/pg", Body::from(serde_json::to_vec(&json!(input)).unwrap())).await;
+        let (status_code, response) = api_call(
+            http::Method::POST,
+            "/api/pg",
+            Body::from(serde_json::to_vec(&json!(input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response);
         assert_eq!(status_code, StatusCode::CREATED);
-        assert_eq!(response.get("name").unwrap().as_str().unwrap(), input.customer_name.as_str());
-        assert_eq!(response.get("surname").unwrap().as_str().unwrap(), input.customer_surname.as_str());
+        assert_eq!(
+            response.get("name").unwrap().as_str().unwrap(),
+            input.customer_name.as_str()
+        );
+        assert_eq!(
+            response.get("surname").unwrap().as_str().unwrap(),
+            input.customer_surname.as_str()
+        );
     }
 
     #[tokio::test]
     async fn get_customer() {
         let input = get_customer_model("Blanche", "Jarvis");
 
-        let (status_code, response)  = api_call(http::Method::POST, "/api/pg", Body::from(serde_json::to_vec(&json!(input)).unwrap())).await;
+        let (status_code, response) = api_call(
+            http::Method::POST,
+            "/api/pg",
+            Body::from(serde_json::to_vec(&json!(input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response);
         assert_eq!(status_code, StatusCode::CREATED);
 
         let id = response.get("id").unwrap().as_str().unwrap();
         let uri = format!("/api/pg/{}", id);
 
-        let (status_code, response_get)  = api_call(http::Method::GET, &uri, Body::empty()).await;
+        let (status_code, response_get) = api_call(http::Method::GET, &uri, Body::empty()).await;
         println!("{:?}", response_get);
         assert_eq!(status_code, StatusCode::OK);
-        assert_eq!(response_get.get("name").unwrap().as_str().unwrap(), input.customer_name.as_str());
-        assert_eq!(response_get.get("surname").unwrap().as_str().unwrap(), input.customer_surname.as_str());
+        assert_eq!(
+            response_get.get("name").unwrap().as_str().unwrap(),
+            input.customer_name.as_str()
+        );
+        assert_eq!(
+            response_get.get("surname").unwrap().as_str().unwrap(),
+            input.customer_surname.as_str()
+        );
     }
 
     #[tokio::test]
     async fn list_customers() {
         let input = get_customer_model("Rafael", "Scott");
 
-        let (status_code, response)  = api_call(http::Method::POST, "/api/pg", Body::from(serde_json::to_vec(&json!(input)).unwrap())).await;
+        let (status_code, response) = api_call(
+            http::Method::POST,
+            "/api/pg",
+            Body::from(serde_json::to_vec(&json!(input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response);
         assert_eq!(status_code, StatusCode::CREATED);
 
-        let (status_code, response)  = api_call(http::Method::GET, "/api/pg", Body::empty()).await;
+        let (status_code, response) = api_call(http::Method::GET, "/api/pg", Body::empty()).await;
         println!("{:?}", response);
         assert_eq!(status_code, StatusCode::OK);
         assert_eq!(response.get("status").unwrap().as_str().unwrap(), "success");
@@ -155,17 +185,25 @@ mod tests {
     async fn delete_customer() {
         let input = get_customer_model("Polly", "Shepard");
 
-        let (status_code, response)  = api_call(http::Method::POST, "/api/pg", Body::from(serde_json::to_vec(&json!(input)).unwrap())).await;
+        let (status_code, response) = api_call(
+            http::Method::POST,
+            "/api/pg",
+            Body::from(serde_json::to_vec(&json!(input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response);
         assert_eq!(status_code, StatusCode::CREATED);
 
         let id = response.get("id").unwrap().as_str().unwrap();
         let uri = format!("/api/pg/{}", id);
 
-        let (status_code, response_get)  = api_call(http::Method::DELETE, &uri, Body::empty()).await;
+        let (status_code, response_get) = api_call(http::Method::DELETE, &uri, Body::empty()).await;
         println!("{:?}", response_get);
         assert_eq!(status_code, StatusCode::OK);
-        assert_eq!(response_get.get("status").unwrap().as_str().unwrap(), "deleted");
+        assert_eq!(
+            response_get.get("status").unwrap().as_str().unwrap(),
+            "deleted"
+        );
         assert_eq!(response_get.get("id").unwrap().as_str().unwrap(), id);
     }
 
@@ -174,81 +212,140 @@ mod tests {
         let original_input = get_customer_model("Polly", "Shepard");
         let modified_input = get_customer_model("Hattie", "Rodgers");
 
-        let (status_code, response_create)  = api_call(http::Method::POST, "/api/pg", Body::from(serde_json::to_vec(&json!(original_input)).unwrap())).await;
+        let (status_code, response_create) = api_call(
+            http::Method::POST,
+            "/api/pg",
+            Body::from(serde_json::to_vec(&json!(original_input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response_create);
         assert_eq!(status_code, StatusCode::CREATED);
 
         let id = response_create.get("id").unwrap().as_str().unwrap();
         let uri = format!("/api/pg/{}", id);
 
-        let (status_code, response_patch)  = api_call(http::Method::PATCH, &uri, Body::from(serde_json::to_vec(&json!(modified_input)).unwrap())).await;
+        let (status_code, response_patch) = api_call(
+            http::Method::PATCH,
+            &uri,
+            Body::from(serde_json::to_vec(&json!(modified_input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response_patch);
         assert_eq!(status_code, StatusCode::OK);
-        assert_eq!(response_patch.get("status").unwrap().as_str().unwrap(), "success");
+        assert_eq!(
+            response_patch.get("status").unwrap().as_str().unwrap(),
+            "success"
+        );
 
-        let (status_code, response_get)  = api_call(http::Method::GET, &uri, Body::empty()).await;
+        let (status_code, response_get) = api_call(http::Method::GET, &uri, Body::empty()).await;
         println!("{:?}", response_get);
         assert_eq!(status_code, StatusCode::OK);
-        assert_eq!(response_get.get("name").unwrap().as_str().unwrap(), modified_input.customer_name.as_str());
-        assert_eq!(response_get.get("surname").unwrap().as_str().unwrap(), modified_input.customer_surname.as_str());
+        assert_eq!(
+            response_get.get("name").unwrap().as_str().unwrap(),
+            modified_input.customer_name.as_str()
+        );
+        assert_eq!(
+            response_get.get("surname").unwrap().as_str().unwrap(),
+            modified_input.customer_surname.as_str()
+        );
     }
-
 
     #[tokio::test]
     async fn create_order() {
         let input = get_order_schema("paul", "banana");
 
-        let (status_code, response)  = api_call(http::Method::POST, "/api/mongo", Body::from(serde_json::to_vec(&json!(input)).unwrap())).await;
+        let (status_code, response) = api_call(
+            http::Method::POST,
+            "/api/mongo",
+            Body::from(serde_json::to_vec(&json!(input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response);
         assert_eq!(status_code, StatusCode::OK);
-        assert_eq!(response["data"]["order"]["customer_name"].as_str().unwrap(), input.customer_name.as_str());
-        assert_eq!(response["data"]["order"]["product_name"].as_str().unwrap(), input.product_name.as_str());
+        assert_eq!(
+            response["data"]["order"]["customer_name"].as_str().unwrap(),
+            input.customer_name.as_str()
+        );
+        assert_eq!(
+            response["data"]["order"]["product_name"].as_str().unwrap(),
+            input.product_name.as_str()
+        );
     }
 
     #[tokio::test]
     async fn get_order() {
         let input = get_order_schema("paul", "banana");
 
-        let (status_code, response_post)  = api_call(http::Method::POST, "/api/mongo", Body::from(serde_json::to_vec(&json!(input)).unwrap())).await;
+        let (status_code, response_post) = api_call(
+            http::Method::POST,
+            "/api/mongo",
+            Body::from(serde_json::to_vec(&json!(input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response_post);
         assert_eq!(status_code, StatusCode::OK);
 
         let id = response_post["data"]["order"]["id"].as_str().unwrap();
         let uri = format!("/api/mongo/{}", id);
 
-        let (status_code, response_get)  = api_call(http::Method::GET, &uri, Body::empty()).await;
+        let (status_code, response_get) = api_call(http::Method::GET, &uri, Body::empty()).await;
         println!("{:?}", response_get);
         assert_eq!(status_code, StatusCode::OK);
-        assert_eq!(response_get["data"]["order"]["customer_name"].as_str().unwrap(), input.customer_name.as_str());
-        assert_eq!(response_get["data"]["order"]["product_name"].as_str().unwrap(), input.product_name.as_str());
+        assert_eq!(
+            response_get["data"]["order"]["customer_name"]
+                .as_str()
+                .unwrap(),
+            input.customer_name.as_str()
+        );
+        assert_eq!(
+            response_get["data"]["order"]["product_name"]
+                .as_str()
+                .unwrap(),
+            input.product_name.as_str()
+        );
     }
 
     #[tokio::test]
     async fn list_orders() {
         let input = get_order_schema("paul", "banana");
 
-        let (status_code, response_post)  = api_call(http::Method::POST, "/api/mongo", Body::from(serde_json::to_vec(&json!(input)).unwrap())).await;
+        let (status_code, response_post) = api_call(
+            http::Method::POST,
+            "/api/mongo",
+            Body::from(serde_json::to_vec(&json!(input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response_post);
         assert_eq!(status_code, StatusCode::OK);
 
-        let (status_code, response_get)  = api_call(http::Method::GET, "/api/mongo", Body::empty()).await;
+        let (status_code, response_get) =
+            api_call(http::Method::GET, "/api/mongo", Body::empty()).await;
         println!("{:?}", response_get);
         assert_eq!(status_code, StatusCode::OK);
-        assert_eq!(response_get.get("status").unwrap().as_str().unwrap(), "success");
+        assert_eq!(
+            response_get.get("status").unwrap().as_str().unwrap(),
+            "success"
+        );
     }
 
     #[tokio::test]
     async fn delete_order() {
         let input = get_order_schema("paul", "banana");
 
-        let (status_code, response_post)  = api_call(http::Method::POST, "/api/mongo", Body::from(serde_json::to_vec(&json!(input)).unwrap())).await;
+        let (status_code, response_post) = api_call(
+            http::Method::POST,
+            "/api/mongo",
+            Body::from(serde_json::to_vec(&json!(input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response_post);
         assert_eq!(status_code, StatusCode::OK);
 
         let id = response_post["data"]["order"]["id"].as_str().unwrap();
         let uri = format!("/api/mongo/{}", id);
 
-        let (status_code, response_delete)  = api_call(http::Method::DELETE, &uri, Body::empty()).await;
+        let (status_code, response_delete) =
+            api_call(http::Method::DELETE, &uri, Body::empty()).await;
         println!("{:?}", response_delete);
         assert_eq!(status_code, StatusCode::OK);
         assert_eq!(response_delete["status"].as_str().unwrap(), "deleted");
@@ -260,23 +357,45 @@ mod tests {
         let original_input = get_order_schema("paul", "banana");
         let modified_input = get_order_schema("mark", "apple");
 
-        let (status_code, response_post)  = api_call(http::Method::POST, "/api/mongo", Body::from(serde_json::to_vec(&json!(original_input)).unwrap())).await;
+        let (status_code, response_post) = api_call(
+            http::Method::POST,
+            "/api/mongo",
+            Body::from(serde_json::to_vec(&json!(original_input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response_post);
         assert_eq!(status_code, StatusCode::OK);
 
         let id = response_post["data"]["order"]["id"].as_str().unwrap();
         let uri = format!("/api/mongo/{}", id);
 
-        let (status_code, response_patch)  = api_call(http::Method::PATCH, &uri, Body::from(serde_json::to_vec(&json!(modified_input)).unwrap())).await;
+        let (status_code, response_patch) = api_call(
+            http::Method::PATCH,
+            &uri,
+            Body::from(serde_json::to_vec(&json!(modified_input)).unwrap()),
+        )
+        .await;
         println!("{:?}", response_patch);
         assert_eq!(status_code, StatusCode::OK);
-        assert_eq!(response_patch.get("status").unwrap().as_str().unwrap(), "success");
+        assert_eq!(
+            response_patch.get("status").unwrap().as_str().unwrap(),
+            "success"
+        );
 
-        let (status_code, response_get)  = api_call(http::Method::GET, &uri, Body::empty()).await;
+        let (status_code, response_get) = api_call(http::Method::GET, &uri, Body::empty()).await;
         println!("{:?}", response_get);
         assert_eq!(status_code, StatusCode::OK);
-        assert_eq!(response_get["data"]["order"]["customer_name"].as_str().unwrap(), modified_input.customer_name.as_str());
-        assert_eq!(response_get["data"]["order"]["product_name"].as_str().unwrap(), modified_input.product_name.as_str());
+        assert_eq!(
+            response_get["data"]["order"]["customer_name"]
+                .as_str()
+                .unwrap(),
+            modified_input.customer_name.as_str()
+        );
+        assert_eq!(
+            response_get["data"]["order"]["product_name"]
+                .as_str()
+                .unwrap(),
+            modified_input.product_name.as_str()
+        );
     }
-
 }
